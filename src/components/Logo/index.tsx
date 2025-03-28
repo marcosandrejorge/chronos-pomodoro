6d22f23068935641e39fcf854cd4fd3a59c9d980
Marcos André Jorge <marcosandre.bnu@gmail.com>
2025-03-28 14:49:49 -0300

    Avanço

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import { TimerIcon } from "lucide-react";
+import styles from "./styles.module.css";
+
+export const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <a href='#' className={styles.logoLink}>
+        <TimerIcon />
+        <span>Chronos</span>
+      </a>
+    </div>
+  );
+};
